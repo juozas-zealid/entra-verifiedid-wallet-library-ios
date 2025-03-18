97f0367d91432ba74824d06c0e5d38628a8e4418
Juozas Bitkevicius <juozas@zealid.com>
2025-03-18 15:53:57 +0200

    Verified id claim fields update

--- WalletLibrary/WalletLibrary/VerifiedId/VerifiedIdClaim.swift
+++ WalletLibrary/WalletLibrary/VerifiedId/VerifiedIdClaim.swift
@@ -6,10 +6,16 @@
 /**
  * A mapping of the claims contained within a Verified Id.
  */
-public struct VerifiedIdClaim {
-    
-    /// The id of the claim. This id can be used as a label to display the claims.
+public struct VerifiedIdClaim
+{
+    /// The id of the claim
     public let id: String
+    
+    /// The label of the claim. This id can be used as a label to display the claims.
+    public let label: String?
+    
+    /// The type of data object the value is.
+    public let type: String?
     
     /// The value of the claim.
     public let value: Any
